b9dbf61fdcc0de6100f747cc6d9ad281cbe169e7
kkbellew <kkbellew@gmail.com>
2023-10-03 15:17:17 -0500

    added var's to js

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"; 
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var symboles = "`~!@#$%^&*()-_+=:;'.,";
+var numbers = "0123456789";
+
 window.alert("Welcome to the password shazam");
-if (window.confirm("Would you like numbers included?")) {
-  window.confirm("Ok you got it!");
+
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
+var userLength = window.prompt("How many characters would you like (8-128)?");
+console.log(userLength);
+if (isNaN(userlength) || userLength <8 || userLength > 128){
+window.alert("How many characters would you like (8-128)?");
+writePassword();
 }
-if (window.confirm("Would you like Uppercase letters included?")) {
-  window.confirm("Alright you're the boss!");
+getUserSelection();
+
+window.confirm("Would you like Uppercase letters included? ok for yes, cancel for no") {
+  if getUserSelection
+  
 }
 if (window.confirm("Would you like Lowercase letters included?")) {
-  window.confirm("Pefect!");
+  
 }
-if (window.confirm("How many characters would you like (8-128)?")) {
-  window.confirm("Pefect!");
+if (window.confirm("Would you like numbers?")) {
+  ;
 }
 if (window.confirm("Would you like special characters included?")) {
   window.confirm("Great! I have everything needed to make you a random password!");
 }
 
 // Write password to the #password input
-new Array(12).fill().map(() => String.fromCharCode(Math.random()*86+40)).join("");
- 
-console.log(writePassword());
+function writePassword(){
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+
+}
+
+var password = generatePassword();
 
 
 // Add event listener to generate button
