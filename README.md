6451fe33d0f0baa6c5684870c55b24e4568d3c3e
kkbellew <kkbellew@gmail.com>
2023-10-05 22:44:41 -0500

    added a readme,added img to read me, more code in js

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # password-shazam
 password generator
+//password generator
+
+//needs to be 8 characters long no more then 128.
+
+//asked for character types to include in the password.
+
+//wheather or not to include uppercae, lowercase letters, numeric, and/or special characters.
+
+//a prompt appears to make sure the user want the charcters.
+
+//then a password is generated.
+
+//either displayed in an alert or written to the page.
+
+![Alt text](image.png)
--- script.js
+++ script.js
@@ -2,46 +2,43 @@
 var generateBtn = document.querySelector("#generate");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz"; 
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var symboles = "`~!@#$%^&*()-_+=:;'.,";
+var specialCharacters = "`~!@#$%^&*()-_+=:;'.,{}[]";
 var numbers = "0123456789";
 
 window.alert("Welcome to the password shazam");
+window.prompt("How many characters would you like (8-128)?");
+window.prompt("Would you like Uppercase letters included?");
+window.prompt("Would you like Lowercase letters included?");
+window.prompt("Would you like numbers?");
+window.prompt("Would you like special characters included?");
 
-var passwordText = document.querySelector("#password");
-passwordText.value = password;
-var userLength = window.prompt("How many characters would you like (8-128)?");
-console.log(userLength);
-if (isNaN(userlength) || userLength <8 || userLength > 128){
-window.alert("How many characters would you like (8-128)?");
-writePassword();
+userLength = window.prompt("How many characters would you like (8-128)?") {
+if (isNaN(userlength) || userLength <8 || userLength > 128)
 }
-getUserSelection();
 
-window.confirm("Would you like Uppercase letters included? ok for yes, cancel for no") {
-  if getUserSelection
-  
+window.alert("How many characters would you like (8-128)?");{
+  getUserSelection();
 }
-if (window.confirm("Would you like Lowercase letters included?")) {
-  
+
+  if window.confirm("Would you like Uppercase letters included?") {
+  choiceArr = choiceArr.concat(lowerCaseArr);
 }
-if (window.confirm("Would you like numbers?")) {
-  ;
+  if (window.confirm("Would you like Lowercase letters included?")) {
+  choiceArr = choiceArr.concat(upperCaseArr);
 }
-if (window.confirm("Would you like special characters included?")) {
+  if (window.confirm("Would you like numbers?")) {
+  choiceArr = choiceArr.concat(numbersArr);
+}
+  if (window.confirm("Would you like special characters included?")) {
   window.confirm("Great! I have everything needed to make you a random password!");
+  choiceArr = choiceArr.concat(specialCharArr);
 }
-
 // Write password to the #password input
-function writePassword(){
-  var password = generatePassword();
-
+function generatePassword(upper, lower, number, length, specialCharacter) {
 }
-
-var password = generatePassword();
-
+console.log(random.string)
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-function displayPassword() {
-  document.querySelector("#generate").innerHTML = password();
-}
+generateBtn.addEventListener("click", ()=>{
+
+})
